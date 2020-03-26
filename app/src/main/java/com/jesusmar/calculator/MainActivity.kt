3456f32c31cd2886b0348d09fb53ede242740365
jesusmarlima <jesusmarlima@gmail.com>
2020-03-26 09:46:49 -0400

    add clear and dot behavior

--- app/src/main/java/com/jesusmar/calculator/MainActivity.kt
+++ app/src/main/java/com/jesusmar/calculator/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +14,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit(view:View){
-        Toast.makeText(this, (view as Button).text, Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        tvInput.text = ""
+    }
+
+    fun onDot(view: View){
+        if (tvInput.text.length > 0
+            && tvInput.text.last() != '.'
+            && !tvInput.text.contains('.', true)) {
+                tvInput.append(".")
+        }
     }
 }
